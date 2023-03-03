401e86cd344e020a804fa13aa186d795c6217c96
ZviadiOlegovich <zo.shchukin@gmail.com>
2023-03-03 15:42:22 +0300

    created branch dev, and two files changes

--- db.py
+++ db.py
@@ -1,3 +1,16 @@
-file = open("dbConfig.txt", encoding= "utf-8")
-t1 = file.readlines()
-print(t1)
+import mysql.connector
+
+# file = open("dbConfig.txt", encoding= "utf-8")
+# t1 = file.readlines()
+
+
+# db = mysql.connector.connect(t1)
+
+# Настройки подключения к базе данных
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Ford2008",
+    database="todo",
+    port="33306"
+)
